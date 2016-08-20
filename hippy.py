7b8c6bc742a4bc25c361e5fdfb2f3b61c86c496c
degrigis <degrigis@gmail.com>
2016-08-21 01:12:38 +0200

    fixed some errors
    Signed-off-by: degrigis <degrigis@gmail.com>

--- hippy.py
+++ hippy.py
@@ -8,11 +8,8 @@ tag_hippy_end   = "</hippy-d75d6fc7>"
 api_call_json = []
 proc_info_json = None
 
-operations = { 'free': free, 'malloc': malloc, 'calloc': calloc, 'realloc': realloc }
-procInfo = None
-timeline = [State()] # a timeline is a list of State
 
-class ProcInfo:
+class ProcInfo():
  def __init__(self,hstart,hend,libcstart,libcend,binaryarch):
   self.architecture         = binaryarch  # binary is 32 or 64 bit?
   self.heap_start_address   = hstart
@@ -21,7 +18,7 @@ class ProcInfo:
   self.glibc_end_address    = libcend
   return
 
- def getArchMutiplier():
+ def getArchMutiplier(self):
      if "x86_64" in self.architecture:
          return 2
      else:
@@ -31,14 +28,14 @@ class ProcInfo:
  This is a list of chunks currently allocated in a State
 '''
 class State(list):
- def __init__():
+
+ def __init__(self):
   self.errors = []
   self.info = []
   self.dump_id = -1 # this in order to correlate a State with a taken dump
-  super().__init__(*args, **kwargs)
   return
 
- def getChunkAt(address):
+ def getChunkAt(self,address):
      for i,chunk in enumerate(self):
          if chunk.addr == address:
             return i
@@ -51,9 +48,9 @@ class Chunk():
      self.raw_addr = hex(int(addr,16) - procInfo.getArchMutiplier() * 8)  # this is the real start address of the chunk
      self.size     = size     # size of the chunk as requested from the user
      self.raw_size = raw_size # raw size of the chunk ( the size returned from usable_size() )
-     self.type     = getChunkType(raw_size)
+     self.type     = self.getChunkType(raw_size)
 
- def getChunkType(raw_size):
+ def getChunkType(self,raw_size):
      raw_size = int(raw_size,10)
      if raw_size >= 8 and raw_size <= 80:
          return "fast_chunk"
@@ -71,6 +68,7 @@ def parseProgramOut(output):
          if dumped_json['type'] == "apicall":
              api_call_json.append(dumped_json)  # append this mini-json in the apicall list
          if dumped_json['type'] == "procinfo":
+             global proc_info_json
              proc_info_json = dumped_json
          print_next_line = 0
      if tag_hippy_start in line:
@@ -97,27 +95,27 @@ def realloc(state,api_args,api_info,api_ret):
     newsize = api_args['size']
 
     if address_to_realloc == "0":
-        malloc(state,api_args,api_info,api_ret):
+        malloc(state,api_args,api_info,api_ret)
     elif newsize == "0":
-        free(state,api_args,None,None):
+        free(state,api_args,None,None)
     else:
         index,res = state.getChunkAt(address_to_realloc) # let's search the chunk that has been reallocated
         if api_ret == address_to_realloc:
-            state(index) = Chunk(api_ret,api_args['size'],api_info['usable_chunk_size'])
+            state[index] = Chunk(api_ret,api_args['size'],api_info['usable_chunk_size'])
         else:
             new_api_args = {}
             new_api_args['address'] = api_info['internal_api_call']['api_args']['address']
             free(state,new_api_args,None,None)
             timeline.append(state) # keep track of this free performed by the realloc
-            malloc(state,api_args,api_info,api_ret):
+            malloc(state,api_args,api_info,api_ret)
 
 def buildTimeline():
     for djson in api_call_json:
         api_name = djson['api_name']
         api_args = djson['api_args']
         api_info = djson.get('api_info',[])
-        api_ret  = djson['api_return']
-        op = operations(api_name)
+        api_ret  = djson.get('api_return',[])
+        op = operations[api_name]
         state = timeline[-1]
         op(state,api_args,api_info,api_ret)
         timeline.append(state)
@@ -127,15 +125,19 @@ def buildTimeline():
  from the json and build the ProcInfo object
 '''
 def buildProcInfo():
-    if proc_info_json.get('heap_range',[]) != []:
-        heap_start_address  = djson['heap_start_address']
-        heap_end_address    = djson['heap_end_address']
+    heap_range = proc_info_json.get('heap_range',[])
+    if  heap_range != []:
+        heap_start_address  = heap_range['heap_start_address']
+        heap_end_address    = heap_range['heap_end_address']
     #if proc_info_json.get('glibc_range',[]) != []:
     #    glibc_start_address = djson['glibc_start_address']
     #    glibc_end_address   = djson['glibc_end_address']
     arch = proc_info_json['arch']
-    return procinfo(heap_start_address,heap_end_address,"","",arch)
+    return ProcInfo(heap_start_address,heap_end_address,"","",arch)
 
+operations = {'free': free, 'malloc': malloc, 'calloc': calloc, 'realloc': realloc}
+procInfo = None
+timeline = [State()]  # a timeline is a list of State
 
 if __name__ == '__main__':
 

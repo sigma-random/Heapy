2f69299e0bb73daf842fc94728ad8c09b6ed0d3a
degrigis <degrigis@gmail.com>
2016-08-29 22:27:26 +0200

    new layout for the gui
    Signed-off-by: degrigis <degrigis@gmail.com>

---
+++ test/trace_me_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+
+char bi[10] = "BBBBBBBBBB";
+char ci[10] = "CCCCCCCCCC";
+char di[10] = "CCCCCCCCCC";
+char ee[10] = "CCCCCCCCCC";
+
+unsigned int p = malloc(10);
+
+free(p);
+
+unsigned int a = malloc(13);
+free(a);
+
+unsigned int c = malloc(20);
+
+unsigned int d = malloc(11);
+
+getchar();
+
+
+
+}

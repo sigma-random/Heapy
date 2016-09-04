35d7681b42d6e2fffb33e15af68b9665d0fadf6b
degrigis <degrigis@gmail.com>
2016-09-04 17:20:02 +0200

    last commit
    Signed-off-by: degrigis <degrigis@gmail.com>

--- test/test_coalesc.c
+++ test/test_coalesc.c
@@ -4,12 +4,9 @@
 int main(){
 
 	
-	unsigned int b = malloc(1000);
 	unsigned int c = malloc(4);
-	unsigned int a = malloc(1000);
-	free(b);
 	free(c);
-	free(a);
+
         unsigned int d = malloc(100);
 
 
